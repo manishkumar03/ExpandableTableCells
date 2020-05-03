4d1aa2b81f3535395186deaace0efab2949ebf82
Manish Kumar <mkumar@acvauctions.com>
2020-05-02 22:22:11 -0400

    Initial commit

---
+++ ExpandableTableCells/ExpandableTableCellViewControllerTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ExpandableTableCellViewControllerTableViewController.swift
+//  ExpandableTableCells
+//
+//  Created by Manish Kumar on 2020-02-11.
+//  Copyright © 2020 Manish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ExpandableTableCellViewControllerTableViewController: UITableViewController {
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.numberOfLines = 3
+        cell.textLabel?.text = """
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        """
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+
+        tableView.beginUpdates()
+        cell?.textLabel?.numberOfLines = (cell?.textLabel?.numberOfLines == 3) ? 0 : 3
+        tableView.endUpdates()
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Expandable TableViewCells
+
+An app showing an alternative way to expand table cells on tap
